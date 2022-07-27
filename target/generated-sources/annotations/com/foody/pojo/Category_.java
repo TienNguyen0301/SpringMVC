874bad9d3a038ed26e03413a4808d9e2ef54b3d7
nguyen tien <tiennh.2001@gmail.com>
2022-07-28 01:28:45 +0700

    SpringMVC

---
+++ target/generated-sources/annotations/com/foody/pojo/Category_.java
@@ -0,0 +1,17 @@
+package com.foody.pojo;
+
+import com.foody.pojo.Food;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-07-27T16:07:03")
+@StaticMetamodel(Category.class)
+public class Category_ { 
+
+    public static volatile SingularAttribute<Category, String> name;
+    public static volatile SingularAttribute<Category, Integer> id;
+    public static volatile CollectionAttribute<Category, Food> foodCollection;
+
+}
